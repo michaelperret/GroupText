552741f91f4ce600a52dab9364f8e3b025d153fd
Rudy Mutter <rudy@yetihq.com>
2014-08-01 16:24:26 -0700

    Update models.py

--- letsparty/models.py
+++ letsparty/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 
 class Address(models.Model):
+    # There's some specific Django fields/libraries for storing location information (State, Zipcode, etc)
     title = models.CharField(max_length=100)
     street = models.CharField(max_length=160, help_text="ex: 225 Maiden Ln")
     city = models.CharField(max_length=160, help_text="ex: San Francisco")
@@ -20,8 +21,10 @@ class Partier(AbstractUser):
 
 
 class Event(models.Model):
+    # Not sure what these 'name' attributes are doing, pretty sure they're unnecessary
     title = models.CharField(max_length=40, name='title')
     date = models.CharField(max_length=30, name='date')
     location = models.ForeignKey(Address, name='location')
     creator = models.ForeignKey(Partier, name='creator')
+    # Not sure what this field is for, but should it be a number field?
     guest_nums = models.TextField(name='guests', null=True)
